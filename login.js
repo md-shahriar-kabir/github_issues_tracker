7ba77b15519c7942d553c1acad8de23b121928bf
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-03-08 00:59:38 +0600

    Login JS Updated

---
+++ login.js
@@ -0,0 +1,17 @@
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+
+const userName = document.getElementById("userName").value;
+const password = document.getElementById("password").value;
+
+if (userName === "admin" && password === "admin123") {
+
+window.location.href = "dashboard.html";
+
+} else {
+
+alert("Wrong username or password");
+
+}
+
+});
